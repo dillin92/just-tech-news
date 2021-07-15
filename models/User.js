9ca90c5b794f1ee220030eb73ec1a2802160e1ce
dillin92 <dillin970@gmail.com>
2021-07-15 15:51:43 -0500

    secured PW with hashing, created login routes

--- models/User.js
+++ models/User.js
@@ -2,7 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 // create our User model
-class User extends Model {}
+class User extends Model {
+
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password) 
+  }
+}
 
 // create fields/columns for User model
 User.init(
